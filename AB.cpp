682df07fe6edb69f89be82a80704e7f63a6d6355
saraseyfii <seyfisara68@gmail.com>
2024-06-13 03:30:22 +0330

    Add files via upload

---
+++ AB.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+	int a, b;
+	cin >> a >> b;
+	if (a >= 0 && b >= o) {
+		cout << "posetive";
+	}
+	if (a < 0 && b < 0) {
+		cout << "negative";
+	}
+	if (a > b) {
+		cout << "a>b";
+	}
+	if (a > 0) {
+		cout << "a+";
+	}
+	else {
+		cout << "a-";
+	} if (b > 0) {
+		cout << "b+";
+	}
+	else {
+		cout"b-";
+		return 0;
+	}
